ed74b496e3cdd5359b0c11c4333d0293f9dafec9
Alisson <arteaga10alisson@gmail.com>
2023-09-14 16:13:35 -0500

    add project and configuration

---
+++ src/test/java/com/aarteaga/gateway/server/SpringbootServicioGatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aarteaga.gateway.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioGatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
